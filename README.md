2771aacc0b487c356d9ab62b425a7b0345d570e7
aligholizade1995 <aligholizade1995@gmail.com>
2022-11-22 03:22:02 +0330

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+v2ray-connection-limiter <br><br>
+(v2.0.1 updated / if you have V2 please update codes on your server)<br>
+with this script you can detect and ban those V2ray accounts which aren't only connected to one devices.
+
+It's simple part i could do for the ppl who providing VPN for iranian users so they could selling VPN for more and more users. so 50 50 WIN WIN ! VPN providers getting money to get more Servers and many users Could get VPN as well lol. we should be togheder right ?
+
+so i made something for VLESS and other protocols (vmess seems blocked in iran) which you can detect those accounts which using by more than 1 IP ! this script might has some bugs and it's possible to get more power from CPU SERVER .! so check everything first and put it on background
+
+how it works ? 
+it's finding connected IPs to user's Port and if more than specific IP counts are connected , it will disable that account . 
+it counts those Ips which connecting and downloading data in same time so it doesn't count standbyed and disconnected connections
+
+<b>Install Guide :</b><br>
+1 - install python .<br>
+2 - pip3 install requests and pip3 install schedule<br>
+3 - install netstat (if your server doesn't have it so install it - debian : apt install net-tools)<br>
+4 - put it on background => nohup python3 main.py &  (without background process : python3 main.py) <br>
+5 - you can set telegram bot token + your tlg chat_id for notification as well . it's pretty clear on the code .
+<br>
+<b><h4>Note: </h4></b><br>
+you can change Limits in line 8 >> _max_allowed_connections = 1 (1 means only one device could able to connect but i suggest to set it on 3 it works great then, becuase sometimes switching between mobileData and ADSL gonna make some issues so num 3 is better .)
+
+<b>Note: </b> : in v2 new users will checked automatically
+
+<b>IMPROVED: </b> and blocked users will bee unblocked when connections reduce to _max_allowed_connections
+
+original developer: https://github.com/net-pioneer
+
+tested on this V2ray: https://seakfind.github.io/2021/10/10/X-UI/
+
+<b>Donate:</b><br>
+Good news ! If you enjoyed this script you could donate me by donating USDT to my wallet ! A Coffee or etc.
+<br><code>USDT wallet Address (TRC20) : TZ3tm395rvKiMjc4S1M2bJ6hXRvUcGQKxp</code>
+
+<br><code>original developer USDT wallet Address (TRC20) : TBFJ3YirXc7vwwuRNeqhcBcQziB3h9bPbs</code>
---
+++ main.py
@@ -0,0 +1,86 @@
+from glob import glob
+from threading import Timer
+import os;
+import sqlite3;
+import time;
+import requests;
+import subprocess;
+import threading;
+import schedule;
+_db_address = '/etc/x-ui/x-ui.db'
+_max_allowed_connections = 1
+_user_last_id = 0
+_telegrambot_token = ''
+_telegram_chat_id = '' # you can get this in @cid_bot bot.
+def getUsers():
+    global _user_last_id
+    conn = sqlite3.connect(_db_address)
+    cursor = conn.execute(f"select id,remark,port from inbounds where id > {_user_last_id}");
+    users_list = [];
+    for c in cursor:
+        users_list.append({'name':c[1],'port':c[2]})
+        _user_last_id = c[0];
+    conn.close();
+    return users_list
+
+def enableAccount(user_port):
+    conn = sqlite3.connect(_db_address)
+    conn.execute(f"update inbounds set enable = 1 where port={user_port}");
+    conn.commit()
+    conn.close();
+    print(f"inbound with port {user_port} unblocked")
+    time.sleep(2)
+    os.popen("x-ui restart")
+    time.sleep(3)
+
+def disableAccount(user_port):
+    conn = sqlite3.connect(_db_address)
+    conn.execute(f"update inbounds set enable = 0 where port={user_port}");
+    conn.commit()
+    conn.close();
+    print(f"inbound with port {user_port} blocked")
+    time.sleep(2)
+    os.popen("x-ui restart")
+    unblocking = Timer(10, enableAccount, [user_port])
+    unblocking.start()
+    time.sleep(3)
+
+def checkNewUsers():
+    conn = sqlite3.connect(_db_address)
+    cursor = conn.execute(f"select count(*) from inbounds WHERE id > {_user_last_id}");
+    new_counts = cursor.fetchone()[0];
+    conn.close();
+    if new_counts > 0:
+        init()
+
+def init():
+    users_list = getUsers();
+    for user in users_list:
+        thread = AccessChecker(user)
+        thread.start()
+        print("starting checker for : " + user['name'])
+class AccessChecker(threading.Thread):
+    def __init__(self, user):
+        threading.Thread.__init__(self)
+        self.user = user;
+    def run(self):
+        #global _max_allowed_connections; <<if you get variable error uncomment this.
+        user_remark = self.user['name'];
+        user_port = self.user['port'];
+        while True:
+            netstate_data =  os.popen("netstat -antu | grep :"+str(user_port)+" | grep -v LISTEN | awk '{print $5}' | cut -d: -f1 | sort | uniq -c").read();
+            netstate_data = str(netstate_data)
+            connection_count =  len(netstate_data.split("\n")) - 1;
+            #print("c "+str(user_port) + "-"+ str(connection_count))
+            if connection_count > _max_allowed_connections:
+                user_remark = user_remark.replace(" ","%20")
+                #requests.get(f'https://api.telegram.org/bot{_telegrambot_token}/sendMessage?chat_id={_telegram_chat_id}&text={user_remark}%20locked')
+                disableAccount(user_port=user_port)
+            else:
+                time.sleep(5)
+
+init();
+schedule.every(10).minutes.do(checkNewUsers)
+while True:
+    schedule.run_pending()
+    time.sleep(1)
